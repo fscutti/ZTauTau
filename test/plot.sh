a917912129f1aed0a82bbad787bc9489da9fe04b
Federico <federicoscutti@gmail.com>
2016-06-13 02:14:32 +0000

    test added

---
+++ test/plot.sh
@@ -0,0 +1,8 @@
+#!bin/bash
+
+# Strings are passed to the script but this is redundant!
+
+python ../ztautau/scripts/merge.py --var="tau_n_tracks" --reg="SR" --lab="TEST REGION" --icut="4" --input="/data/laram1/ztautau/Hists_allregions" --output="./" --makeplot=True
+
+
+
