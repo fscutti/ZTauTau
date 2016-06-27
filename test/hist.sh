1c9c233a4b1627ecbe7931c36db4821a1fcddd60
Federico <federicoscutti@gmail.com>
2016-06-27 10:30:07 +0000

    hist.sh

--- test/hist.sh
+++ test/hist.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 ## Batch
-python ../ssdilep/run/j.plotter_VR_TwoMu.py --input /data/fscutti/ssdilep/menu_singlemu/merged/nominal/physics_Main_00276262.root --sampletype="data"
-#python ../ZprimeTauTauHadHad2012/run/j.plotter.py \
+python ../ztautau/run/j.plotter.py --input /data/fscutti/shared/v03/merged/nominal/physics_Main_00276262.root --sampletype="data"
+#python ../ztautau/run/j.plotter.py \
 #--input /lustre/atlas/group/higgs/TauTauHadHad/TNT/full/batch_p1443_current_merged/nominal/ttbar_allhad.root --sampletype="mc" ##--config="sys:FWEIGHT_UP"
 
-#python ../ZprimeTauTauHadHad2012/run/j.plotter.py \
+#python ../ztautau/run/j.plotter.py \
 #--input /lustre/atlas/group/higgs/TauTauHadHad/TNT/full/test_tnt_ggHtautauhh_MA400TB20.root --sampletype="mc" 
