5a3ed096a82950174a663346d26eaa5d1744d4b6
Lara Mason <laram1@vm-115-146-87-73.melbourne.rc.nectar.org.au>
2016-06-28 07:18:50 +0000

    plot.sh

--- test/plot.sh
+++ test/plot.sh
@@ -2,7 +2,25 @@
 
 # Strings are passed to the script but this is redundant!
 
-python ../ztautau/scripts/merge.py --var="tau_n_tracks" --reg="SR" --lab="TEST REGION" --icut="4" --input="/data/laram1/ztautau/Hists_allregions" --output="./" --makeplot=True
+#python ../ztautau/scripts/merge.py --var="tau_pt" --reg="SR" --lab="TEST REGION" --icut="4" --input="/data/laram1/ztautau/HistPLATYPUS" --output="./" --makeplot=True
+#python ../ztautau/scripts/merge.py --var="tau_pt" --reg="SR_25med" --lab="TEST REGION" --icut="5" --input="/data/laram1/ztautau/HistPLATYPUS" --output="./" --makeplot=False
+#python ../ztautau/scripts/merge.py --var="tau_pt" --reg="SR_35med" --lab="TEST REGION" --icut="5" --input="/data/laram1/ztautau/HistEMU" --output="./35med" --makeplot=False
+
+#     ONE PRONG  
+
+#python ../ztautau/scripts/merge.py --var="tau_pt" --reg="SR_Tau1Track" --lab="TEST" --icut="5" --input="/data/laram1/ztautau/HistKOALA" --output="./" --makeplot=False
+#python ../ztautau/scripts/merge.py --var="tau_pt" --reg="SR_25med_Tau1Track" --lab="TEST REGION" --icut="6" --input="/data/laram1/ztautau/HistKOALA" --output="./" --makeplot=False
+#python ../ztautau/scripts/merge.py --var="tau_pt" --reg="SR_35med_Tau1Track" --lab="TEST REGION" --icut="6" --input="/data/laram1/ztautau/HistEMU" --output="./35med" --makeplot=False
+
+#     THREE PRONG  
+
+#python ../ztautau/scripts/merge.py --var="tau_pt" --reg="SR_Tau3Track" --lab="TEST" --icut="5" --input="/data/laram1/ztautau/HistKOALA" --output="./" --makeplot=False
+#python ../ztautau/scripts/merge.py --var="tau_pt" --reg="SR_25med_Tau3Track" --lab="TEST REGION" --icut="6" --input="/data/laram1/ztautau/HistKOALA" --output="./" --makeplot=False
+python ../ztautau/scripts/merge.py --var="tau_pt" --reg="SR_35med_Tau3Track" --lab="TEST REGION" --icut="6" --input="/data/laram1/ztautau/HistEMU" --output="./35med" --makeplot=False
+
+
+
+
 
 
 
