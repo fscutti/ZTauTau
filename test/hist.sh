e60827c7c436dd2c5fadcdff621a007952658b58
Lara Mason <laram1@vm-115-146-84-190.melbourne.rc.nectar.org.au>
2016-07-18 10:17:33 +0000

    hist.sh

--- test/hist.sh
+++ test/hist.sh
@@ -2,5 +2,6 @@
 
 ## Batch
 #python ../ztautau/run/j.plotter.py --input /data/fscutti/shared/v03/merged/nominal/physics_Main_00276073.root --sampletype="data"
-python ../ztautau/run/j.plotter.py --input /data/fscutti/shared/v03/merged/nominal/PoPy8_Zmumu.root --sampletype="mc"
-#python ../ztautau/run/j.plotter.py --input /data/fscutti/shared/merged/nominal/physics_Main_00276778.root --sampletype="data"
+#python ../ztautau/run/j.plotter.py --input /data/fscutti/shared/v03/merged/nominal/PoPy8_Zmumu.root --sampletype="mc"
+python ../ztautau/run/j.plotter.py --input /coepp/cephfs/mel/laram1/data16/nominal/physics_Main_00298773.root --sampletype="data"
+#python ../ztautau/run/j.plotter.py --input /coepp/cephfs/mel/laram1/data16/nominal/PoPy8_Zmumu.root --sampletype="mc"
