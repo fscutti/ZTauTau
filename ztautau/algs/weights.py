9ead01a2025aa272a838568b3b8837e0857685b1
Lara Mason <laram1@vm-115-146-84-190.melbourne.rc.nectar.org.au>
2016-08-01 09:34:19 +0000

    weights.py

--- ztautau/algs/weights.py
+++ ztautau/algs/weights.py
@@ -114,12 +114,13 @@ class MuonSF(pyframe.core.Algorithm):
     def execute(self, weight):
         sf=1.0
         if "mc" in self.sampletype: 
-          #sf *= self.chain.lep_0_NOMINAL_MuEffSF_Reco_QualMedium #lep_0_NOMINAL_effSF_RecoMedium
-          #sf *= lep_0_NOMINAL_HLT_mu20_iloose_OR_HLT_mu40_MU_TRIG_QUAL_MEDIUM_MU_TRIG_ISO_GRADIENT #2015
-          #sf *= self.chain.lep_0_NOMINAL_MuEffSF_HLT_mu24_imedium_OR_HLT_mu50_QualMedium_IsoGradient  #2016
-          #sf *= self.chain.lep_0_NOMINAL_MuEffSF_IsoGradient
-          #print "pt(mu) %lf, trig SF %lf, reco SF %lf ", % (self.chain.lep_0_pt, self.chain.lep_0_NOMINAL_MuEffSF_HLT_mu24_imedium_OR_HLT_mu50_QualMedium_IsoIsoGradient, self.chain.lep_0_NOMINAL_MuEffSF_Reco_QualMedium)
+          sf *= self.chain.lep_0_NOMINAL_MuEffSF_Reco_QualMedium #lep_0_NOMINAL_effSF_RecoMedium
+          #sf *= self.chain.lep_0_NOMINAL_MuEffSF_HLT_mu20_iloose_L1MU15_OR_HLT_mu40_QualMedium_IsoIsoGradient#2015
+          sf *= self.chain.lep_0_NOMINAL_MuEffSF_HLT_mu24_imedium_OR_HLT_mu50_QualMedium_IsoGradient  #2016
+          sf *= self.chain.lep_0_NOMINAL_MuEffSF_IsoGradient
           # just add sf *= lep_0_NOMINAL_my_super_fancy_weight
+          print "pt(mu) %lf, trig SF %lf, reco SF %lf " % (self.chain.lep_0_pt, self.chain.lep_0_NOMINAL_MuEffSF_HLT_mu20_iloose_L1MU15_OR_HLT_mu40_QualMedium_IsoIsoNone, self.chain.lep_0_NOMINAL_MuEffSF_Reco_QualMedium) 
+	  #print self.chain.lep_0_NOMINAL_MuEffSF_HLT_mu24_imedium_OR_HLT_mu50_QualMedium_IsoGradient 
           if self.scale: 
                if self.scale=='up': pass
                elif self.scale=='dn': pass
@@ -156,9 +157,12 @@ class MuonSFIsoGrad(pyframe.core.Algorithm):
     def execute(self, weight):
         sf=1.0
         if "mc" in self.sampletype:
-          #sf *= self.chain.lep_0_NOMINAL_MuEffSF_Reco_QualMedium
-          #sf *= self.chain.lep_0_NOMINAL_MuEffSF_HLT_mu24_imedium_OR_HLT_mu50_QualMedium_IsoNone
-          #print "pt(mu) %lf, trig SF %lf, reco SF %lf ", %(self.chain.lep_0_pt, self.chain.lep_0_NOMINAL_MuEffSF_HLT_mu24_imedium_OR_HLT_mu50_QualMedium_IsoIsoNone, self.chain.lep_0_NOMINAL_MuEffSF_Reco_QualMedium)          
+          sf *= self.chain.lep_0_NOMINAL_MuEffSF_Reco_QualMedium
+          sf *= self.chain.lep_0_NOMINAL_MuEffSF_HLT_mu24_imedium_OR_HLT_mu50_QualMedium_IsoNone #2016
+          # WRONG sf *= lep_0_NOMINAL_HLT_mu20_iloose_OR_HLT_mu40_MU_TRIG_QUAL_MEDIUM_MU_TRIG_ISO_NONE #2015
+
+	  #sf *= self.chain.lep_0_NOMINAL_MuEffSF_HLT_mu20_iloose_L1MU15_OR_HLT_mu40_QualMedium_IsoIsoNone
+	  print "pt(mu) %lf, antiIso trig SF %lf, reco SF %lf " % (self.chain.lep_0_pt, self.chain.lep_0_NOMINAL_MuEffSF_HLT_mu20_iloose_L1MU15_OR_HLT_mu40_QualMedium_IsoIsoNone, self.chain.lep_0_NOMINAL_MuEffSF_Reco_QualMedium)          
           # just add sf *= lep_0_NOMINAL_my_super_fancy_weight
           if self.scale:
                if self.scale=='up': pass
