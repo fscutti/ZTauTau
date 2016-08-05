0f7e431b1e80ee44bd0c2d314a09dc1c3831131c
Lara Mason <laram1@vm-115-146-84-190.melbourne.rc.nectar.org.au>
2016-08-05 04:34:13 +0000

    plot.sh

--- test/plot.sh
+++ test/plot.sh
@@ -2,8 +2,8 @@
 
 # Strings are passed to the script but this is redundant!
 
-#python ../ztautau/scripts/merge.py --var="tau_pt" --reg="SR" --lab="TEST REGION" --icut="4" --input="/coepp/cephfs/mel/laram1/ztautau/TESTKangaroo" --output="./" --makeplot=False
-python ../ztautau/scripts/merge.py --var="mu_pt" --reg="SR" --lab="TEST REGION" --icut="4" --input="/coepp/cephfs/mel/laram1/ztautau/HISTPossum" --output="./" --makeplot=True
+python ../ztautau/scripts/merge.py --var="tau_pt" --reg="SR" --lab="TEST REGION" --icut="4" --input="/coepp/cephfs/mel/laram1/ztautau/HISTv19_2016" --output="./" --makeplot=True
+#python ../ztautau/scripts/merge.py --var="mu_pt" --reg="SR" --lab="TEST REGION" --icut="4" --input="/coepp/cephfs/mel/laram1/ztautau/HISTv19_2016" --output="./" --makeplot=True
 #python ../ztautau/scripts/merge.py --var="tau_pt" --reg="SR_25med" --lab="TEST REGION" --icut="5" --input="/coepp/cephfs/mel/laram1/ztautau/TESTKangaroo" --output="./" --makeplot=False
 #python ../ztautau/scripts/merge.py --var="tau_pt" --reg="SR_35med" --lab="TEST REGION" --icut="5" --input="/data/laram1/ztautau/HistEMU" --output="./35med" --makeplot=False
 
