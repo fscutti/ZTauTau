7992734d71da523284a50666641800af95d742f8
Lara Mason <laram1@vm-115-146-84-190.melbourne.rc.nectar.org.au>
2016-08-05 04:33:17 +0000

    weights.py

--- ztautau/algs/weights.py
+++ ztautau/algs/weights.py
@@ -115,13 +115,17 @@ class MuonSF(pyframe.core.Algorithm):
         sf=1.0
         if "mc" in self.sampletype: 
 
+	  #sf=1.0
+
           sf *= self.chain.lep_0_NOMINAL_MuEffSF_Reco_QualMedium #lep_0_NOMINAL_effSF_RecoMedium
           sf *= self.chain.lep_0_NOMINAL_MuEffSF_IsoGradient
 
-          #sf *= self.chain.lep_0_NOMINAL_MuEffSF_HLT_mu20_iloose_L1MU15_OR_HLT_mu40_QualMedium_IsoIsoGradient#2015
-          sf *= self.chain.lep_0_NOMINAL_MuEffSF_HLT_mu24_imedium_OR_HLT_mu50_QualMedium_IsoGradient  #2016
+	  #sf *= self.chain.lep_0_NOMINAL_MuEffSF_HLT_mu20_iloose_L1MU15_OR_HLT_mu40_QualMedium_IsoIsoGradient # v12 2015
+          #sf *= self.chain.lep_0_NOMINAL_MuEffSF_HLT_mu20_iloose_L1MU15_OR_HLT_mu40_QualMedium_IsoGradient # 2015
+          sf *= self.chain.lep_0_NOMINAL_MuEffSF_HLT_mu24_imedium_OR_HLT_mu50_QualMedium_IsoGradient  # 2016
  
-	  #print "pt(mu) %lf, trig SF %lf, reco SF %lf " % (self.chain.lep_0_pt, self.chain.lep_0_NOMINAL_MuEffSF_HLT_mu24_imedium_OR_HLT_mu50_QualMedium_IsoGradient, self.chain.lep_0_NOMINAL_MuEffSF_Reco_QualMedium) 
+	  #print "pt(mu) %lf, trig SF %lf, reco SF %lf, isoGrad SF %lf" % (self.chain.lep_0_pt, self.chain.lep_0_NOMINAL_MuEffSF_HLT_mu24_imedium_OR_HLT_mu50_QualMedium_IsoGradient, self.chain.lep_0_NOMINAL_MuEffSF_Reco_QualMedium, self.chain.lep_0_NOMINAL_MuEffSF_IsoGradient)
+ 
           if self.scale: 
                if self.scale=='up': pass
                elif self.scale=='dn': pass
@@ -159,12 +163,15 @@ class MuonSFIsoGrad(pyframe.core.Algorithm):
         sf=1.0
         if "mc" in self.sampletype:
 
+	  #sf=1.0
+
           sf *= self.chain.lep_0_NOMINAL_MuEffSF_Reco_QualMedium
 
-          #sf *= self.chain.lep_0_NOMINAL_MuEffSF_HLT_mu20_iloose_L1MU15_OR_HLT_mu40_QualMedium_IsoIsoNone #2015
-          sf *= self.chain.lep_0_NOMINAL_MuEffSF_HLT_mu24_imedium_OR_HLT_mu50_QualMedium_IsoNone #2016
+	  #sf *= self.chain.lep_0_NOMINAL_MuEffSF_HLT_mu20_iloose_L1MU15_OR_HLT_mu40_QualMedium_IsoIsoNone # v12 2015
+          #sf *= self.chain.lep_0_NOMINAL_MuEffSF_HLT_mu20_iloose_L1MU15_OR_HLT_mu40_QualMedium_IsoNone # 2015
+          sf *= self.chain.lep_0_NOMINAL_MuEffSF_HLT_mu24_imedium_OR_HLT_mu50_QualMedium_IsoNone # 2016
 
-	  #print "pt(mu) %lf, antiIso trig SF %lf, reco SF %lf " % (self.chain.lep_0_pt, self.chain.lep_0_NOMINAL_MuEffSF_HLT_mu20_iloose_L1MU15_OR_HLT_mu40_QualMedium_IsoIsoNone, self.chain.lep_0_NOMINAL_MuEffSF_Reco_QualMedium)          
+	  #print "pt(mu) %lf, antiIso trig SF %lf, reco SF %lf " % (self.chain.lep_0_pt, self.chain.lep_0_NOMINAL_MuEffSF_HLT_mu24_imedium_OR_HLT_mu50_QualMedium_IsoNone, self.chain.lep_0_NOMINAL_MuEffSF_Reco_QualMedium)          
 
           if self.scale:
                if self.scale=='up': pass
@@ -203,8 +210,17 @@ class TauSF(pyframe.core.Algorithm):
         sf=1.0
         if "mc" in self.sampletype: 
  
-          sf *= self.chain.tau_0_NOMINAL_TauEffSF_selection #v19
+	  #sf=1.0
+
+          #sf *= self.chain.tau_0_NOMINAL_TauEffSF_selection #v19
 	  #sf *= self.chain.tau_0_NOMINAL_TAU_EFF_SELECTION #v12 
+
+	  sf *= self.chain.tau_0_NOMINAL_TauEffSF_JetBDTmedium
+	  sf *= self.chain.tau_0_NOMINAL_TauEffSF_reco
+	  sf *= self.chain.tau_0_NOMINAL_TauEffSF_HadTauEleOLR_tauhad
+	  
+	  #print "tau(mu) %lf, tau id trig SF %lf, tau reco SF %lf, tau olr SF %lf " % (self.chain.tau_0_pt, self.chain.tau_0_NOMINAL_TauEffSF_JetBDTmedium, self.chain.tau_0_NOMINAL_TauEffSF_reco, self.chain.tau_0_NOMINAL_TauEffSF_HadTauEleOLR_tauhad)
+
           if self.scale: 
                if self.scale=='up': pass
                elif self.scale=='dn': pass
