58620fbc4beb5d6a8f5af50c86a86e06a68b790e
Lara Mason <laram1@vm-115-146-84-190.melbourne.rc.nectar.org.au>
2016-08-12 05:31:39 +0000

    SubmitPlots.py

--- batch/SubmitPlots.py
+++ batch/SubmitPlots.py
@@ -17,8 +17,8 @@ ana      = 'ztautau'
 
 #indir    = 'HistPOSSUM_2016_rqcdsyst'
 #outdir   = 'PlotsPOSSUM_2106_rqcdsyst'
-indir = 'TESTPossum_1'
-outdir = 'PlotsPossum_withtop'
+indir = 'HISTv19_2015_ttb'
+outdir = 'PLOTSv19_2015_ttb'
 
 USER    = os.getenv('USER')
 MAIN    = os.getenv('MAIN')
@@ -60,10 +60,10 @@ regions = {}
 
 regions["SR"] 		= [4,"SR"]
 
-#regions['SR_Tau3Track'] = [5,'SR_Tau3Track']
-#regions['SR_Tau1Track'] = [5,'SR_Tau1Track']
+regions['SR_Tau3Track'] = [5,'SR_Tau3Track']
+regions['SR_Tau1Track'] = [5,'SR_Tau1Track']
 
-#regions["SR_25med"] 	= [5,"SR_25med"]
+regions["SR_25med"] 	= [5,"SR_25med"]
 
 #regions['SR_25med_Tau1Track'] = [6,'SR_25med_Tau1Track']
 #regions["SR_25med_Tau3Track"] = [6,"SR_25med_Tau3Track"]
@@ -105,8 +105,9 @@ regions['SR_SS_highPT_Tau3Track'] = [6,'SR_SS_highPT_Tau3Track']
 regions['SR_SS_lowPT_Tau3Track'] = [6,'SR_SS_lowPT_Tau3Track']
 """
 # WJETS SS --------------------------------------
-"""
+
 regions["Wjets_SS"] 	   = [3, "Wjets_SS"]
+"""
 regions["Wjets_SS_Tau1Track"] = [4,"Wjets_SS_Tau1Track"]
 regions["Wjets_SS_Tau3Track"] = [4,"Wjets_SS_Tau3Track"]
 
@@ -115,14 +116,15 @@ regions["Wjets_SS_25med"]  = [4, "Wjets_SS_25med"]
 #regions["Wjets_SS_35med_Tau1Track"]  = [5, "Wjets_SS_35med_Tau1Track"]
 #regions["Wjets_SS_35med_Tau3Track"]  = [5, "Wjets_SS_35med_Tau3Track"]
 
-
+"""
 regions["Wjets_SS_highPT"] = [4, "Wjets_SS_highPT"]
 regions["Wjets_SS_lowPT"]  = [4, "Wjets_SS_lowPT"]
-
+"""
 
 # WJETS OS --------------------------------------
-
+"""
 regions["Wjets_OS"]	   = [3, "Wjets_OS"]
+"""
 regions["Wjets_OS_Tau1Track"] = [4,"Wjets_OS_Tau1Track"]
 regions["Wjets_OS_Tau3Track"] = [4,"Wjets_OS_Tau3Track"]
 
@@ -131,10 +133,10 @@ regions["Wjets_OS_25med"]  = [4, "Wjets_SS_25med"]
 #regions["Wjets_OS_35med_Tau1Track"]  = [5, "Wjets_3S_35med_Tau1Track"]
 #regions["Wjets_OS_35med_Tau3Track"]  = [5, "Wjets_3S_35med_Tau3Track"]
 
-
+"""
 regions["Wjets_OS_highPT"] = [4, "Wjets_OS_highPT"]
 regions["Wjets_OS_lowPT"]  = [4, "Wjets_OS_lowPT"]
-
+"""
 
 # ANTI ISO SS -----------------------------------
 
@@ -150,8 +152,9 @@ regions["AntiIsoCR_SS_highPT"] = [3, "AntiIso_SS_highPT"]
 regions["AntiIsoCR_SS_lowPT"]  = [3, "AntiIso_SS_lowPT"]
 
 # ANTI ISO OS -----------------------------------
-
+"""
 regions["AntiIsoCR_OS"]	       = [2, "AntiIso_OS"]
+"""
 regions["AntiIsoCR_OS_Tau1Track"] = [3,"AntiIso_OS_Tau1Track"]
 regions["AntiIsoCR_OS_Tau3Track"] = [3,"AntiIso_OS_Tau3Track"]
 
@@ -159,10 +162,10 @@ regions["AntiIsoCR_OS_25med"]  = [3, "AntiIso_OS_25med"]
 #regions["AntiIsoCR_OS_35med_Tau1Track"]  = [4, "AntiIso_OS_35med_Tau1Track"]
 #regions["AntiIsoCR_OS_35med_Tau3Track"]  = [4, "AntiIso_OS_25med_Tau3Track"]
 
-
+"""
 regions["AntiIsoCR_OS_highPT"] = [3, "AntiIso_OS_highPT"]
 regions["AntiIsoCR_OS_lowPT"]  = [3, "AntiIso_OS_lowPT"]
-"""
+
 
 #---------------------
 # Make input tarball
