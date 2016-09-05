547aa2805534279e01d8d9bf97ac17812cae8a02
Lara Mason <laram1@vm-115-146-84-190.melbourne.rc.nectar.org.au>
2016-09-05 00:27:10 +0000

    sys update

--- ztautau/scripts/systematics.py
+++ ztautau/scripts/systematics.py
@@ -33,16 +33,65 @@ class Systematic(object):
 sys_dict = {}
 
 # just an example
-SYS1 = sys_dict['SYS1'] = Systematic(
-        'SYS1',
-        var_up='SYS1_UP',
-        var_dn='SYS1_DN'
+
+#['METResoPara', 'METResoPerp', 'MUID_DN', 'MUID_UP', 'MUMS_DN', 'MUMS_UP', 'MUSCALE_DN', 'MUSCALE_UP', 'TAUSF_SYS_DN', 'TAUSF_SYS_UP', 'TAUSF_STAT_UP', 'TAUSF_STAT_DN', 'MUSF_SYS_DN', 'MUSF_SYS_UP', 'MUSF_STAT_UP', 'MUSF_STAT_DN', 'METSCALE_DN', 'METSCALE_UP']
+
+"""
+MUID = sys_dict['MUID'] = Systematic(
+        'MUID',
+        var_up='MUID_UP', #name of submit hist output
+        var_dn='MUID_DN'
         )
+
+MUMS = sys_dict['MUMS'] = Systematic(
+        'MUMS',
+        var_up='MUMS_UP', #name of submit hist output
+        var_dn='MUMS_DN'
+        )
+
+MUSCALE = sys_dict['MUSCALE'] = Systematic(
+        'MUSCALE',
+        var_up='MUSCALE_UP', #name of submit hist output
+        var_dn='MUSCALE_DN'
+        )
+
+METSCALE = sys_dict['METSCALE'] = Systematic(
+        'METSCALE',
+        var_up='METSCALE_UP', #name of submit hist output
+        var_dn='METSCALE_DN'
+        )
+"""
+TAUSF_SYS = sys_dict['TAUSF_SYS'] = Systematic(
+        'TAUSF_SYS',
+        var_up='TAUSF_SYS_UP', #name of submit hist output
+        var_dn='TAUSF_SYS_DN'
+        )
+
+TAUSF_STAT = sys_dict['TAUSF_STAT'] = Systematic(
+        'TAUSF_STAT',
+        var_up='TAUSF_STAT_UP', #name of submit hist output
+        var_dn='TAUSF_STAT_DN'
+        )
+"""
+MUSF_SYS = sys_dict['MUSF_SYS'] = Systematic(
+        'MUSF_SYS',
+        var_up='MUSF_SYS_UP', #name of submit hist output
+        var_dn='MUSF_SYS_DN'
+        )
+
+MUSF_STAT = sys_dict['MUSF_STAT'] = Systematic(
+        'MUSF_STAT',
+        var_up='MUSF_STAT_UP', #name of submit hist output
+        var_dn='MUSF_STAT_DN'
+        )
+
+"""
+"""
 SYS2 = sys_dict['SYS2'] = Systematic(
         'SYS2','$\\sigma_{\\rm Wtaunu}$',      
         flat_err=0.05,
         )
-
+"""
 # -----------------------------------
 # A whole bunch of RQCD uncertainties
 # -----------------------------------
@@ -66,17 +115,17 @@ RQCD_AntiIsoCR_highPT = sys_dict['RQCD_AntiIsoCR_highPT'] = Systematic(
 
 RQCD_AntiIsoCR_25med = sys_dict['RQCD_AntiIsoCR_25med'] = Systematic(
         'RQCD_AntiIsoCR_25med','$\\sigma_{R_{\\rm QCD}}$',      
-        flat_err=0.05, # some 5% unc.
+        flat_err=0.106746370623,
         )
 
 RQCD_AntiIsoCR_25med_lowPT = sys_dict['RQCD_AntiIsoCR_25med_lowPT'] = Systematic(
         'RQCD_AntiIsoCR_25med_lowPT','$\\sigma_{R_{\\rm QCD}}$',      
-        flat_err=0.05, # some 5% unc.
+        flat_err=0.0645446507515,
         )
 
 RQCD_AntiIsoCR_25med_highPT = sys_dict['RQCD_AntiIsoCR_25med_highPT'] = Systematic(
         'RQCD_AntiIsoCR_25med_highPT','$\\sigma_{R_{\\rm QCD}}$',      
-        flat_err=0.05, # some 5% unc.
+        flat_err=0.0301302931596,
         )
 
 RQCD_AntiIsoCR_35med = sys_dict['RQCD_AntiIsoCR_35med'] = Systematic(
