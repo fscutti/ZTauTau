4990efcb9008539a7c31dde6db4bb7983a97e212
Lara Mason <laram1@vm-115-146-84-190.melbourne.rc.nectar.org.au>
2016-09-05 01:03:17 +0000

    sys

--- test/plot.sh
+++ test/plot.sh
@@ -2,27 +2,32 @@
 
 # Strings are passed to the script but this is redundant!
 
-#python ../ztautau/scripts/merge.py --var="topoetcone20pt" --reg="SR_no_iso" --lab="TEST REGION" --icut="3" --input="/coepp/cephfs/mel/laram1/ztautau/TEST_no_iso" --output="./" --makeplot=False
-python ../ztautau/scripts/merge.py --var="tau_pt" --reg="SR_25med" --lab="TEST REGION" --icut="5" --input="/coepp/cephfs/mel/laram1/ztautau/HistKOALA_new" --output="./" --makeplot=True
-#python ../ztautau/scripts/merge.py --var="tau_pt" --reg="SR_25med" --lab="TEST REGION" --icut="5" --input="/coepp/cephfs/mel/laram1/ztautau/HISTv19_2016_ttb" --output="./" --makeplot=False
-#python ../ztautau/scripts/merge.py --var="tau_pt" --reg="SR_35med" --lab="TEST REGION" --icut="5" --input="/coepp/cephfs/mel/laram1/ztautau/HISTv19_2015_ttb" --output="./" --makeplot=False
+python ../ztautau/scripts/merge.py --var="tau_pt" --reg="SR" --lab="TEST REGION" --icut="4" --input="/coepp/cephfs/mel/laram1/ztautau/TEST_0209" --output="./" --makeplot=True
+#python ../ztautau/scripts/merge.py --var="tau_pt" --reg="SR_Tau1Track" --lab="TEST REGION" --icut="5" --input="/coepp/cephfs/mel/laram1/ztautau/HistKOALA_new" --output="./" --makeplot=True
+#python ../ztautau/scripts/merge.py --var="tau_pt" --reg="SR_25med" --lab="TEST REGION" --icut="5" --input="/coepp/cephfs/mel/laram1/ztautau/TEST_0209" --output="./" --makeplot=False
+#python ../ztautau/scripts/merge.py --var="tau_pt" --reg="SR_OS_no_cuts" --lab="TEST REGION" --icut="0" --input="/coepp/cephfs/mel/laram1/ztautau/HistEMU_new" --output="./" --makeplot=True
 
 #     ONE PRONG  
 
-#python ../ztautau/scripts/merge.py --var="tau_pt" --reg="SR_Tau1Track" --lab="TEST" --icut="5" --input="/coepp/cephfs/mel/laram1/ztautau/HISTv19_2016_ttb" --output="./" --makeplot=False
-#python ../ztautau/scripts/merge.py --var="tau_pt" --reg="SR_25med_Tau1Track" --lab="TEST REGION" --icut="6" --input="/coepp/cephfs/mel/laram1/ztautau/HISTv19_2016_ttb" --output="./" --makeplot=False
+#python ../ztautau/scripts/merge.py --var="tau_pt" --reg="SR_Tau1Track" --lab="TEST" --icut="5" --input="/coepp/cephfs/mel/laram1/ztautau/HistECHIDNA" --output="./" --makeplot=True
+#python ../ztautau/scripts/merge.py --var="tau_pt" --reg="SR_25med_Tau1Track" --lab="TEST REGION" --icut="6" --input="/coepp/cephfs/mel/laram1/ztautau/HistEMU_new" --output="./" --makeplot=True
 #python ../ztautau/scripts/merge.py --var="tau_pt" --reg="SR_35med_Tau1Track" --lab="TEST REGION" --icut="6" --input="/coepp/cephfs/mel/laram1/ztautau/HISTv19_2015_ttb" --output="./" --makeplot=False
 
 #     THREE PRONG  
 
 #python ../ztautau/scripts/merge.py --var="tau_pt" --reg="SR_Tau3Track" --lab="TEST" --icut="5" --input="/coepp/cephfs/mel/laram1/ztautau/HISTv19_2016_ttb" --output="./" --makeplot=False
-#python ../ztautau/scripts/merge.py --var="tau_pt" --reg="SR_25med_Tau3Track" --lab="TEST REGION" --icut="6" --input="/coepp/cephfs/mel/laram1/ztautau/HISTv19_2016_ttb" --output="./" --makeplot=False
+#python ../ztautau/scripts/merge.py --var="tau_pt" --reg="SR_25med_Tau3Track" --lab="TEST REGION" --icut="6" --input="/coepp/cephfs/mel/laram1/ztautau/HistEMU_new" --output="./" --makeplot=True
 #python ../ztautau/scripts/merge.py --var="tau_pt" --reg="SR_35med_Tau3Track" --lab="TEST REGION" --icut="6" --input="/coepp/cephfs/mel/laram1/ztautau/HISTv19_2015_ttb" --output="./" --makeplot=False
 #python ../ztautau/scripts/merge.py --var="m_trans" --reg="AntiIsoCR_OS" --lab="AntiIso" --icut="2" --input="/coepp/cephfs/mel/laram1/ztautau/HISTv19_2016_ttb" --output="./" --makeplot=True
 #python ../ztautau/scripts/merge.py --var="m_trans" --reg="Wjets_OS" --lab="WJets" --icut="3" --input="/coepp/cephfs/mel/laram1/ztautau/HISTv19_2016_ttb" --output="./" --makeplot=True
 
-
-
+#SYSTEMATICS
+#python ../ztautau/scripts/merge.py --var="tau_pt" --reg="SR" --lab="TEST REGION" --icut="4" --input="/coepp/cephfs/mel/laram1/ztautau/TEST_0209/TAUSF_SYS_DN" --output="./" --makeplot=False
+#python ../ztautau/scripts/merge.py --var="tau_pt" --reg="SR_25med" --lab="TEST REGION" --icut="5" --input="/coepp/cephfs/mel/laram1/ztautau/TEST_0209/TAUSF_SYS_DN" --output="./" --makeplot=False
+#
+#python ../ztautau/scripts/merge.py --var="tau_pt" --reg="SR_Tau1Track" --lab="TEST" --icut="5" --input="/coepp/cephfs/mel/laram1/ztautau/TEST_0209/MUID_UP" --output="./" --makeplot=True
+#python ../ztautau/scripts/merge.py --var="tau_pt" --reg="SR_25med_Tau1Track" --lab="TEST REGION" --icut="6" --input="/coepp/cephfs/mel/laram1/ztautau/TEST_0209/MUID_UP" --output="./" --makeplot=True
+#
 
 
 
