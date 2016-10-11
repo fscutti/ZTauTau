2e9e634b8baf28887921b431c81ac27255abaffd
Lara Mason <laram1@vm-115-146-84-202.melbourne.rc.nectar.org.au>
2016-10-11 00:18:28 +0000

    wjets method

--- ztautau/scripts/funcs.py
+++ ztautau/scripts/funcs.py
@@ -127,6 +127,27 @@ def make_stat_scatter(hist1, hist2):
 		graph.SetPointError(i-1,ex,ex,stat,stat)
 	    return graph
 #____________________________________________________________
+def make_graph_from_hist(hist):
+    graph = ROOT.TGraphAsymmErrors()
+    for i in range(1,hist.GetNbinsX()+1):
+		ex = hist.GetBinWidth(i)/2.
+                graph.SetPoint(i-1,hist.GetBinCenter(i),hist.GetBinContent(i))
+                graph.SetPointError(i-1,ex,ex,0,0)
+    return graph
+
+#____________________________________________________________
+def make_band_scatter(hist1, h_UP,h_DN=None):
+    graph = ROOT.TGraphAsymmErrors()
+    if hist1.GetNbinsX() == h_UP.GetNbinsX():
+            for i in range(1,hist1.GetNbinsX()+1):
+        	eUP = abs(h_UP.GetBinContent(i))
+	        eDN = abs(h_UP.GetBinContent(i))
+	        if h_DN: eDN = abs(h_DN.GetBinContent(i))
+                ex = hist1.GetBinWidth(i)/2.
+                graph.SetPoint(i-1,hist1.GetBinCenter(i),hist1.GetBinContent(i))
+                graph.SetPointError(i-1,ex,ex,eDN,eUP)
+            return graph
+#____________________________________________________________
 def make_band_graph_from_hist(h_UP,h_DN=None):
     '''
     makes band graph from hist.
@@ -141,7 +162,7 @@ def make_band_graph_from_hist(h_UP,h_DN=None):
         if h_DN: eDN = abs(h_DN.GetBinContent(i))
         ex = h_UP.GetBinWidth(i)/2.
         graph.SetPoint(i-1,h_UP.GetBinCenter(i),1.)
-        graph.SetPointError(i-1,ex,ex,eUP,eDN)
+        graph.SetPointError(i-1,ex,ex,eDN,eUP)
     return graph
 
 
@@ -154,7 +175,7 @@ def make_error_scatter_graph(hist,h_UP,h_DN):
         eDN = abs(h_DN.GetBinContent(i))
 	ex = h_UP.GetBinWidth(i)/2.
 	graph.SetPoint(i-1,hist.GetBinCenter(i),hist.GetBinContent(i))
-	graph.SetPointError(i-1,ex,ex,eUP,eDN)
+	graph.SetPointError(i-1,ex,ex,eDN,eUP)
     return graph
 #____________________________________________________________
 def get_total_stat_sys_hists(hists,sys_dict):
@@ -230,6 +251,8 @@ def plot_hist(
     blind         = None,
     xmin          = None,
     xmax          = None,
+    xmin2 	  = None,
+    xmax2 	  = None,
     rebin         = None,
     sys_dict      = None,
     do_ratio_plot = False,
@@ -245,7 +268,7 @@ def plot_hist(
     '''
     print 'making plot: ', histname, ' in region', region
     
-    #assert signal, "ERROR: no signal provided for plot_hist"
+	    #assert signal, "ERROR: no signal provided for plot_hist"
     assert backgrounds, "ERROR: no background provided for plot_hist"
  
     if signal:samples = backgrounds + signal    
@@ -255,251 +278,298 @@ def plot_hist(
 
     ## generate nominal hists
     hists = get_hists(
-        region=region,
-        icut=icut,
-        histname=histname,
-        samples=samples,
-        rebin=rebin,
-        sys_dict=sys_dict,
-        )
+	region=region,
+	icut=icut,
+	histname=histname,
+	samples=samples,
+	rebin=rebin,
+	sys_dict=sys_dict,
+	)
     ## sum nominal background
     h_samp_list = []
     for s in backgrounds+signal:
       if not s in hists.keys(): continue
       #print hists[s]
       h_samp_list.append(hists[s])
-      
+      if s.name == "Wjets":
+	h_wjets = hists[s]
+	print "got the", s.name, "hist"
+  
     h_total = histutils.add_hists(h_samp_list)
 
-    ## get stat / sys bands
-    if sys_dict: 
-        total_hists = get_total_stat_sys_hists(h_samp_list,sys_dict)
-        
-        g_stat = make_band_graph_from_hist(total_hists[0])
-        g_stat.SetFillColor(ROOT.kRed-4)
-	g_stat.SetFillStyle(3001)
-        g_tot  = make_band_graph_from_hist(total_hists[3],total_hists[4])
-        g_tot.SetFillColor(ROOT.kAzure+6)
-	g_tot.SetFillStyle(3001)
+    if xmin2:
+        reg = region
+        if not reg: reg = ""
+        name = '_'.join([reg,histname]).replace('/','_')
+        cname = "c_final_%s"%name
+        c = ROOT.TCanvas(cname,cname,800,700)
+
+	center_pad = ROOT.TPad("center_pad", "center_pad", 0.0, 0.0, 0.6, 0.6)
+ 	center_pad.Draw()
+	right_pad = ROOT.TPad("right_pad", "right_pad", 0.55, 0.0, 1.0, 0.6)
+	right_pad.Draw()
+	top_pad = ROOT.TPad("top_pad", "top_pad", 0.0, 0.55, 0.6, 1.0)
+	top_pad.Draw()
+
+	projh2X = h_wjets.ProjectionX()
+	projh2Y = h_wjets.ProjectionY()
+
+	center_pad.cd()
+	h_wjets.Draw("CONT1Z")		
+	c_factor = h_wjets.GetCorrelationFactor(1,2)
+
+	print "***********************************************"
+	print "correlation factor for", histname, "is", c_factor
+	print "***********************************************"
+
+	top_pad.cd()
+	projh2X.SetFillColor(ROOT.kYellow-3)
+	projh2X.Draw("bar")
+
+	right_pad.cd()
+	projh2Y.SetFillColor(ROOT.kYellow-3)
+	projh2Y.Draw("hbar")
+
+	c.cd()
+
+        print 'saving plot...'
+        c.SaveAs("%s.eps"%c.GetName())
+        fout = ROOT.TFile.Open(plotsfile,'UPDATE')
+        fout.WriteTObject(c)
+        fout.Close()
+
     else:
-        h_total_stat = make_stat_hist(h_total)
-        g_stat = make_band_graph_from_hist(h_total_stat)
-        g_stat.SetFillColor(ROOT.kRed-4)
-        g_stat.SetFillStyle(3001)
-        g_tot = None
+	    print "making a 1D plot for you!"
 
-    ## blind data and create ratio 
-    h_data  = None
-    h_ratio = None
-    if data: 
-        h_data = hists[data]
-        if blind: apply_blind(h_data,blind)
-        h_ratio = h_data.Clone('%s_ratio'%(h_data.GetName()))
-        h_ratio.Divide(h_total)
-	h_ratio.SetMarkerStyle(20)
-    
-    yaxistitle = None
-    for b in reversed(backgrounds):
-      if not b in hists.keys(): continue
-      else : 
-        yaxistitle = hists[b].GetYaxis().GetTitle()
-        break
+	    ## get stat / sys bands
+	    if sys_dict: 
+		total_hists = get_total_stat_sys_hists(h_samp_list,sys_dict)
+		
+		g_stat = make_band_graph_from_hist(total_hists[0])
+		g_stat.SetFillColor(ROOT.kRed-4)
+		g_stat.SetFillStyle(3001)
+		g_tot  = make_band_graph_from_hist(total_hists[3],total_hists[4])
+		g_tot.SetFillColor(ROOT.kAzure+6)
+		g_tot.SetFillStyle(3001)
+	    else:
+		h_total_stat = make_stat_hist(h_total)
+		g_stat = make_band_graph_from_hist(h_total_stat)
+		g_stat.SetFillColor(ROOT.kRed-4)
+		g_stat.SetFillStyle(3001)
+		g_tot = None
 
-    ## create stack
-    h_stack = ROOT.THStack()
-    for b in reversed(signal+backgrounds):
-      if not b in hists.keys(): continue
-      h_stack.Add(hists[b])
-   
-    nLegend = len(signal+backgrounds) + 1
-    x_legend = 0.63
-    x_leg_shift = -0.055
-    y_leg_shift = 0.0 
-    legYCompr = 8.0
-    legYMax = 0.85
-    legYMin = legYMax - (legYMax - (0.55 + y_leg_shift)) / legYCompr * nLegend
-    legXMin = x_legend + x_leg_shift
-    legXMax = legXMin + 0.4
-  
-    ## create legend (could use metaroot functionality?)
-    if not do_ratio_plot:
-      legXMin -= 0.005
-      legXMax -= 0.058
-    leg = ROOT.TLegend(legXMin,legYMin,legXMax,legYMax)
-    leg.SetBorderSize(0)
-    leg.SetFillColor(0)
-    leg.SetFillStyle(0)
-    if data: leg.AddEntry(h_data,data.tlatex,'PL')
-    for s in signal:
-      sig_tag = s.tlatex
-      if sig_rescale: sig_tag = "%d #times "%int(sig_rescale) + sig_tag
-      if not s in hists.keys(): continue
-      leg.AddEntry(hists[s],sig_tag,'F')
-    for b in backgrounds: 
-      if not b in hists.keys(): continue
-      leg.AddEntry(hists[b],b.tlatex,'F')
-    if sys_dict:
-    	leg.AddEntry(g_tot, "MC Sys.+Stat.", 'F')
-    leg.AddEntry(g_stat, "MC Stat.", 'F')
+	    ## blind data and create ratio 
+	    h_data  = None
+	    h_ratio = None
+	    if data: 
+		h_data = hists[data]
+		if blind: apply_blind(h_data,blind)
+		h_ratio = h_data.Clone('%s_ratio'%(h_data.GetName()))
+		h_ratio.Divide(h_total)
+		h_ratio.SetMarkerStyle(20)
+	    
+	    yaxistitle = None
+	    for b in reversed(backgrounds):
+	      if not b in hists.keys(): continue
+	      else : 
+		yaxistitle = hists[b].GetYaxis().GetTitle()
+		break
 
-    ## create canvas
-    reg = region
-    if not reg: reg = ""
-    name = '_'.join([reg,histname]).replace('/','_') 
-    cname = "c_final_%s"%name
-    if do_ratio_plot: c = ROOT.TCanvas(cname,cname,750,800)
-    else: c = ROOT.TCanvas(cname,cname,800,700)
-    if xmin==None: xmin = h_total.GetBinLowEdge(1)
-    if xmax==None: xmax = h_total.GetBinLowEdge(h_total.GetNbinsX()+1)
-    ymin = 1.e-3
-    ymax = h_total.GetMaximum()
-    for s in signal:
-      if not s in hists.keys(): continue
-      ymax = max([ymax,hists[s].GetMaximum()])
-    if data: ymax = max([ymax,h_data.GetMaximum()])
-    if log: ymax *= 100000.
-    else:   ymax *= 1.8
-    xtitle = h_total.GetXaxis().GetTitle()
+	    ## create stack
+	    h_stack = ROOT.THStack()
+	    for b in reversed(signal+backgrounds):
+	      if not b in hists.keys(): continue
+	      h_stack.Add(hists[b])
+	   
+	    nLegend = len(signal+backgrounds) + 1
+	    x_legend = 0.63
+	    x_leg_shift = -0.055
+	    y_leg_shift = 0.0 
+	    legYCompr = 8.0
+	    legYMax = 0.85
+	    legYMin = legYMax - (legYMax - (0.55 + y_leg_shift)) / legYCompr * nLegend
+	    legXMin = x_legend + x_leg_shift
+	    legXMax = legXMin + 0.4
+	  
+	    ## create legend (could use metaroot functionality?)
+	    if not do_ratio_plot:
+	      legXMin -= 0.005
+	      legXMax -= 0.058
+	    leg = ROOT.TLegend(legXMin,legYMin,legXMax,legYMax)
+	    leg.SetBorderSize(0)
+	    leg.SetFillColor(0)
+	    leg.SetFillStyle(0)
+	    if data: leg.AddEntry(h_data,data.tlatex,'PL')
+	    for s in signal:
+	      sig_tag = s.tlatex
+	      if sig_rescale: sig_tag = "%d #times "%int(sig_rescale) + sig_tag
+	      if not s in hists.keys(): continue
+	      leg.AddEntry(hists[s],sig_tag,'F')
+	    for b in backgrounds: 
+	      if not b in hists.keys(): continue
+	      leg.AddEntry(hists[b],b.tlatex,'F')
+	    if sys_dict:
+		leg.AddEntry(g_tot, "MC Sys.+Stat.", 'F')
+	    leg.AddEntry(g_stat, "MC Stat.", 'F')
 
-    if do_ratio_plot: rsplit = 0.3
-    else: rsplit = 0.
-    pad1 = ROOT.TPad("pad1","top pad",0.,rsplit,1.,1.)
-    pad1.SetLeftMargin(0.15)
-    pad1.SetTicky()
-    pad1.SetTickx()
-    if do_ratio_plot: pad1.SetBottomMargin(0.04)
-    else: pad1.SetBottomMargin(0.15)
+	    ## create canvas
+	    reg = region
+	    if not reg: reg = ""
+	    name = '_'.join([reg,histname]).replace('/','_') 
+	    cname = "c_final_%s"%name
+	    if do_ratio_plot: c = ROOT.TCanvas(cname,cname,750,800)
+	    else: c = ROOT.TCanvas(cname,cname,800,700)	
+	    if xmin==None: xmin = h_total.GetBinLowEdge(1)
+	    if xmax==None: xmax = h_total.GetBinLowEdge(h_total.GetNbinsX()+1)
+	    ymin = 1.e-3
+	    ymax = h_total.GetMaximum()
+	    for s in signal:
+	      if not s in hists.keys(): continue
+	      ymax = max([ymax,hists[s].GetMaximum()])
+	    if data: ymax = max([ymax,h_data.GetMaximum()])
+	    if log: ymax *= 100000.
+	    else:   ymax *= 1.8
+	    xtitle = h_total.GetXaxis().GetTitle()
 
-    pad1.Draw()
-    if do_ratio_plot:
-      pad2 = ROOT.TPad("pad2","bottom pad",0,0,1,rsplit)
-      pad2.SetTopMargin(0.04)
-      pad2.SetBottomMargin(0.40)
-      pad2.SetLeftMargin(0.15)
-      pad2.SetTicky()
-      pad2.SetTickx()
-      pad2.SetGridy()
-    #if do_ratio_plot: pad2.Draw()
-      pad2.Draw()
-    pad1.cd()
+	    if do_ratio_plot: rsplit = 0.3
+	    else: rsplit = 0.
+	    pad1 = ROOT.TPad("pad1","top pad",0.,rsplit,1.,1.)
+	    pad1.SetLeftMargin(0.15)
+	    pad1.SetTicky()
+	    pad1.SetTickx()
+	    if do_ratio_plot: pad1.SetBottomMargin(0.04)
+	    else: pad1.SetBottomMargin(0.15)
 
-    ytitle = "Events" 
-    if not rebin: ytitle = yaxistitle
-    elif rebin!=1:
-      if not "BDT" in xtitle:
-        ytitle += " / %s"%rebin
-        if ("eta" in xtitle) or ("phi" in xtitle) or ("trk" in xtitle): pass
-        else: ytitle += " GeV"
-      else: ytitle += " / %s"%(0.05)
+	    pad1.Draw()
+	    if do_ratio_plot:
+	      pad2 = ROOT.TPad("pad2","bottom pad",0,0,1,rsplit)
+	      pad2.SetTopMargin(0.04)
+	      pad2.SetBottomMargin(0.40)
+	      pad2.SetLeftMargin(0.15)
+	      pad2.SetTicky()
+	      pad2.SetTickx()
+	      pad2.SetGridy()
+	    #if do_ratio_plot: pad2.Draw()
+	      pad2.Draw()
+	    pad1.cd()
 
-    fr1 = pad1.DrawFrame(xmin,ymin,xmax,ymax,';%s;%s'%(xtitle,ytitle))
-    if do_ratio_plot:
-      fr1.GetXaxis().SetTitleSize(0)
-      fr1.GetXaxis().SetLabelSize(0)
-    xaxis1 = fr1.GetXaxis()
-    yaxis1 = fr1.GetYaxis()
-    scale = (1.3+rsplit)
+	    ytitle = "Events" 
+	    if not rebin: ytitle = yaxistitle
+	    elif rebin!=1:
+	      if not "BDT" in xtitle:
+		ytitle += " / %s"%rebin
+		if ("eta" in xtitle) or ("phi" in xtitle) or ("trk" in xtitle): pass
+		else: ytitle += " GeV"
+	      else: ytitle += " / %s"%(0.05)
 
-    if not do_ratio_plot:
-      xaxis1.SetTitleSize( xaxis1.GetTitleSize() * scale )
-      xaxis1.SetLabelSize( 0.9 * xaxis1.GetLabelSize() * scale )
-      xaxis1.SetTickLength( xaxis1.GetTickLength() * scale )
-      xaxis1.SetTitleOffset( 1.3* xaxis1.GetTitleOffset() / scale  )
-      xaxis1.SetLabelOffset( 1.* xaxis1.GetLabelOffset() / scale )
+	    fr1 = pad1.DrawFrame(xmin,ymin,xmax,ymax,';%s;%s'%(xtitle,ytitle))
+	    if do_ratio_plot:
+	      fr1.GetXaxis().SetTitleSize(0)
+	      fr1.GetXaxis().SetLabelSize(0)
+	    xaxis1 = fr1.GetXaxis()
+	    yaxis1 = fr1.GetYaxis()
+	    scale = (1.3+rsplit)
 
-    yaxis1.SetTitleSize( yaxis1.GetTitleSize() * scale )
-    yaxis1.SetTitleOffset( 2.1 * yaxis1.GetTitleOffset() / scale )
-    yaxis1.SetLabelSize( 0.8 * yaxis1.GetLabelSize() * scale )
-    yaxis1.SetLabelOffset( 1. * yaxis1.GetLabelOffset() / scale )
-    xaxis1.SetNdivisions(510)
-    yaxis1.SetNdivisions(510)
+	    if not do_ratio_plot:
+	      xaxis1.SetTitleSize( xaxis1.GetTitleSize() * scale )
+	      xaxis1.SetLabelSize( 0.9 * xaxis1.GetLabelSize() * scale )
+	      xaxis1.SetTickLength( xaxis1.GetTickLength() * scale )
+	      xaxis1.SetTitleOffset( 1.3* xaxis1.GetTitleOffset() / scale  )
+	      xaxis1.SetLabelOffset( 1.* xaxis1.GetLabelOffset() / scale )
 
-    h_stack.Draw("SAME,HIST")
-    
-    """
-    for s in reversed(signal):
-      if not s in hists.keys(): continue
-      if sig_rescale: hists[s].Scale(sig_rescale)
-      hists[s].Draw("SAME,HIST")
-    """
-    
-    if data: h_data.Draw("SAME")
-    pad1.SetLogy(log)
-    pad1.SetLogx(logx)
-    leg.Draw()
-    pad1.RedrawAxis()
+	    yaxis1.SetTitleSize( yaxis1.GetTitleSize() * scale )
+	    yaxis1.SetTitleOffset( 2.1 * yaxis1.GetTitleOffset() / scale )
+	    yaxis1.SetLabelSize( 0.8 * yaxis1.GetLabelSize() * scale )
+	    yaxis1.SetLabelOffset( 1. * yaxis1.GetLabelOffset() / scale )
+	    xaxis1.SetNdivisions(510)
+	    yaxis1.SetNdivisions(510)
 
-    tlatex = ROOT.TLatex()
-    tlatex.SetNDC()
-    tlatex.SetTextSize(0.05)
-    lx = 0.6 # for ATLAS internal
-    ly = 0.845
-    tlatex.SetTextFont(42)
-    
-    ty = 0.96
-    th = 0.07
-    tx = 0.18
-    lumi = backgrounds[0].estimator.hm.target_lumi/1000.
-    textsize = 0.8
-    if not do_ratio_plot: textsize = 0.8
-    latex_y = ty-2.*th
-    tlatex.DrawLatex(tx,latex_y,'#scale[%lf]{#scale[%lf]{#int}L dt = %2.1f fb^{-1}, #sqrt{s} = 13 TeV}'%(textsize,0.8*textsize,lumi) )
-    if label:
-      latex_y -= 0.06
-      #for i,line in enumerate(label):
-      #  tlatex.DrawLatex(tx,latex_y-i*0.06,"#scale[%lf]{%s}"%(textsize,line))
-      tlatex.DrawLatex(tx,latex_y - 0.06,"#scale[%lf]{%s}"%(textsize,label))
-    if blind:
-        line = ROOT.TLine()
-        line.SetLineColor(ROOT.kBlack)
-        line.SetLineStyle(2)
-        line.DrawLine(blind,ymin,blind,ymax)
-        bltext = ROOT.TLatex()
-        bltext.SetTextFont(42)
-        bltext.SetTextSize(0.04)
-        bltext.SetTextAngle(90.)
-        bltext.SetTextAlign(31)
-        bltext.DrawLatex(blind,ymax, 'Blind   ')
+	    h_stack.Draw("SAME,HIST")
+	    
+	    """
+	    for s in reversed(signal):
+	      if not s in hists.keys(): continue
+	      if sig_rescale: hists[s].Scale(sig_rescale)
+	      hists[s].Draw("SAME,HIST")
+	    """
+	    
+	    if data: h_data.Draw("SAME")
+	    pad1.SetLogy(log)
+	    pad1.SetLogx(logx)
+	    leg.Draw()
+	    pad1.RedrawAxis()
 
-    if do_ratio_plot:
-      pad2.cd()
-      fr2 = pad2.DrawFrame(xmin,0.49,xmax,1.51,';%s;Data / Bkg_{SM}'%(xtitle))
-      xaxis2 = fr2.GetXaxis()
-      yaxis2 = fr2.GetYaxis()
-      scale = (1. / rsplit)
-      yaxis2.SetTitleSize( yaxis2.GetTitleSize() * scale )
-      yaxis2.SetLabelSize( yaxis2.GetLabelSize() * scale )
-      yaxis2.SetTitleOffset( 2.1* yaxis2.GetTitleOffset() / scale  )
-      yaxis2.SetLabelOffset(0.4 * yaxis2.GetLabelOffset() * scale )
-      xaxis2.SetTitleSize( xaxis2.GetTitleSize() * scale )
-      xaxis2.SetLabelSize( 0.8 * xaxis2.GetLabelSize() * scale )
-      xaxis2.SetTickLength( xaxis2.GetTickLength() * scale )
-      xaxis2.SetTitleOffset( 3.2* xaxis2.GetTitleOffset() / scale  )
-      xaxis2.SetLabelOffset( 2.5* xaxis2.GetLabelOffset() / scale )
-      yaxis2.SetNdivisions(510)
-      xaxis2.SetNdivisions(510)
+	    tlatex = ROOT.TLatex()
+	    tlatex.SetNDC()
+	    tlatex.SetTextSize(0.05)
+	    lx = 0.6 # for ATLAS internal
+	    ly = 0.845
+	    tlatex.SetTextFont(42)
+	    
+	    ty = 0.96
+	    th = 0.07
+	    tx = 0.18
+	    lumi = backgrounds[0].estimator.hm.target_lumi/1000.
+	    textsize = 0.8
+	    if not do_ratio_plot: textsize = 0.8
+	    latex_y = ty-2.*th
+	    tlatex.DrawLatex(tx,latex_y,'#scale[%lf]{#scale[%lf]{#int}L dt = %2.1f fb^{-1}, #sqrt{s} = 13 TeV}'%(textsize,0.8*textsize,lumi) )
+	    if label:
+	      latex_y -= 0.06
+	      #for i,line in enumerate(label):
+	      #  tlatex.DrawLatex(tx,latex_y-i*0.06,"#scale[%lf]{%s}"%(textsize,line))
+	      tlatex.DrawLatex(tx,latex_y - 0.06,"#scale[%lf]{%s}"%(textsize,label))
+	    if blind:
+		line = ROOT.TLine()
+		line.SetLineColor(ROOT.kBlack)
+		line.SetLineStyle(2)
+		line.DrawLine(blind,ymin,blind,ymax)
+		bltext = ROOT.TLatex()
+		bltext.SetTextFont(42)
+		bltext.SetTextSize(0.04)
+		bltext.SetTextAngle(90.)
+		bltext.SetTextAlign(31)
+		bltext.DrawLatex(blind,ymax, 'Blind   ')
 
-      if logx: 
-        pad2.SetLogx(logx) 
-        xaxis2.SetMoreLogLabels()
-      else: 
-        pass
+	    if do_ratio_plot:
+	      pad2.cd()
+	      fr2 = pad2.DrawFrame(xmin,0.49,xmax,1.51,';%s;Data / Bkg_{SM}'%(xtitle))
+	      xaxis2 = fr2.GetXaxis()
+	      yaxis2 = fr2.GetYaxis()
+	      scale = (1. / rsplit)
+	      yaxis2.SetTitleSize( yaxis2.GetTitleSize() * scale )
+	      yaxis2.SetLabelSize( yaxis2.GetLabelSize() * scale )
+	      yaxis2.SetTitleOffset( 2.1* yaxis2.GetTitleOffset() / scale  )
+	      yaxis2.SetLabelOffset(0.4 * yaxis2.GetLabelOffset() * scale )
+	      xaxis2.SetTitleSize( xaxis2.GetTitleSize() * scale )
+	      xaxis2.SetLabelSize( 0.8 * xaxis2.GetLabelSize() * scale )
+	      xaxis2.SetTickLength( xaxis2.GetTickLength() * scale )
+	      xaxis2.SetTitleOffset( 3.2* xaxis2.GetTitleOffset() / scale  )
+	      xaxis2.SetLabelOffset( 2.5* xaxis2.GetLabelOffset() / scale )
+	      yaxis2.SetNdivisions(510)
+	      xaxis2.SetNdivisions(510)
 
-      if g_tot: 
-         g_tot.Draw("E2")
-         g_stat.Draw("E2 Same")
+	      if logx: 
+		pad2.SetLogx(logx) 
+		xaxis2.SetMoreLogLabels()
+	      else: 
+		pass
 
-      else: g_stat.Draw("E2")
+	      if g_tot: 
+		 g_tot.Draw("E2")
+		 g_stat.Draw("E2 Same")
 
-      if data: h_ratio.Draw("SAME") 
-      pad2.RedrawAxis()
+	      else: g_stat.Draw("E2")
 
-    print 'saving plot...'
-    if not log: c.SaveAs("%s.eps"%c.GetName())
-    else:   c.SaveAs("%s_LOG.eps"%c.GetName())
-    fout = ROOT.TFile.Open(plotsfile,'UPDATE')
-    fout.WriteTObject(c)
-    fout.Close()
+	      if data: h_ratio.Draw("SAME") 
+	      pad2.RedrawAxis()
+
+	    print 'saving plot...'
+	    if not log: c.SaveAs("%s.eps"%c.GetName())
+	    else:   c.SaveAs("%s_LOG.eps"%c.GetName())
+	    fout = ROOT.TFile.Open(plotsfile,'UPDATE')
+	    fout.WriteTObject(c)
+	    fout.Close()
 
 #____________________________________________________________
 def write_hist(
