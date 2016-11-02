0e25d13b9acfa4ffd3f6c6dd17d672c749f8052c
Lara Mason <laram1@vm-115-146-84-202.melbourne.rc.nectar.org.au>
2016-11-02 02:59:00 +0000

    update

--- ztautau/algs/weights.py
+++ ztautau/algs/weights.py
@@ -77,6 +77,29 @@ class Pileup(pyframe.core.Algorithm):
         return True
 
 #------------------------------------------------------------------------------
+class JetsSF(pyframe.core.Algorithm):
+    """
+    Multiply by scale factor for jets
+    """
+    #__________________________________________________________________________
+    def __init__(self, cutflow=None,key=None, scale=None, sys_name=None):
+        pyframe.core.Algorithm.__init__(self, name="JetsSF", isfilter=True)
+        self.cutflow = cutflow
+        self.key = key
+	self.scale = scale
+	self.sys_name = sys_name
+    #_________________________________________________________________________
+    def execute(self, weight):
+        if "mc" in self.sampletype: 
+            jetssf = self.chain.jet_NOMINAL_global_effSF_MVX#*self.chain.jet_NOMINAL_global_ineffSF_MVX
+            if self.key: self.store[self.key] = jetssf
+            self.set_weight(jetssf*weight)
+            	
+        return True
+
+#------------------------------------------------------------------------------
+
+
 class MCEventWeight(pyframe.core.Algorithm):
     """
     multiply event weight by MC weight
@@ -125,8 +148,10 @@ class MuonSF(pyframe.core.Algorithm):
     def execute(self, weight):
         sf=1.0
         if "mc" in self.sampletype: 
+          run_number = self.chain.NOMINAL_pileup_random_run_number
 
 	  #sf=1.0
+	  sf*= self.chain.lep_0_NOMINAL_MuEffSF_TTVA
 
           sf *= self.chain.lep_0_NOMINAL_MuEffSF_Reco_QualMedium #lep_0_NOMINAL_effSF_RecoMedium
           sf *= self.chain.lep_0_NOMINAL_MuEffSF_IsoGradient
@@ -135,7 +160,17 @@ class MuonSF(pyframe.core.Algorithm):
           #sf *= self.chain.lep_0_NOMINAL_MuEffSF_HLT_mu20_iloose_L1MU15_OR_HLT_mu40_QualMedium_IsoGradient # 2015
 
           #sf *= self.chain.lep_0_NOMINAL_MuEffSF_HLT_mu24_imedium_OR_HLT_mu50_QualMedium_IsoGradient  # 2016
- 	  sf *= self.chain.lep_0_NOMINAL_MuEffSF_HLT_mu24_imedium_OR_HLT_mu50_QualMedium_IsoNone # v22 2016
+ 	  #sf *= self.chain.lep_0_NOMINAL_MuEffSF_HLT_mu26_ivarmedium_OR_HLT_mu50_QualMedium_IsoNone # v22 2016
+
+          if run_number >= 276262 and run_number <= 284484:
+          	sf *= self.chain.lep_0_NOMINAL_MuEffSF_HLT_mu20_iloose_L1MU15_OR_HLT_mu40_QualMedium_IsoNone # 2015
+          elif run_number >= 296939 and run_number <= 300287:
+                sf *= self.chain.lep_0_NOMINAL_MuEffSF_HLT_mu24_imedium_OR_HLT_mu40_QualMedium_IsoNone # 2016
+          elif run_number >= 300345 and run_number <= 302872:
+                sf *= self.chain.lep_0_NOMINAL_MuEffSF_HLT_mu24_imedium_OR_HLT_mu50_QualMedium_IsoNone # 2016
+          elif run_number >= 302919: 
+          	sf *= self.chain.lep_0_NOMINAL_MuEffSF_HLT_mu26_ivarmedium_OR_HLT_mu50_QualMedium_IsoNone # 2016
+	  #print "run number", run_number
 
 	  #print "pt(mu) %lf, trig SF %lf, reco SF %lf, isoGrad SF %lf" % (self.chain.lep_0_pt, self.chain.lep_0_NOMINAL_MuEffSF_HLT_mu24_imedium_OR_HLT_mu50_QualMedium_IsoGradient, self.chain.lep_0_NOMINAL_MuEffSF_Reco_QualMedium, self.chain.lep_0_NOMINAL_MuEffSF_IsoGradient)
  
@@ -143,23 +178,67 @@ class MuonSF(pyframe.core.Algorithm):
                sf = 1.0
                if self.sys_name == 'MUSF_STAT_UP':
                 sf *= self.chain.lep_0_MUON_EFF_STAT_1up_MuEffSF_Reco_QualMedium
+                sf *= self.chain.lep_0_MUON_TTVA_STAT_1up_MuEffSF_TTVA
+
+                if run_number >= 276262 and run_number <= 284484:
+                        sf *= self.chain.lep_0_MUON_EFF_TrigStatUncertainty_1up_MuEffSF_HLT_mu20_iloose_L1MU15_OR_HLT_mu40_QualMedium_IsoNone # 2015
+                if run_number >= 296939 and run_number <= 300287:
+                        sf *= self.chain.lep_0_MUON_EFF_TrigStatUncertainty_1up_MuEffSF_HLT_mu24_imedium_OR_HLT_mu40_QualMedium_IsoNone # 2016
+                if run_number >= 300345 and run_number <= 302872:
+                        sf *= self.chain.lep_0_MUON_EFF_TrigStatUncertainty_1up_MuEffSF_HLT_mu24_imedium_OR_HLT_mu50_QualMedium_IsoNone # 2016
+                if run_number >= 302919:
+                        sf *= self.chain.lep_0_MUON_EFF_TrigStatUncertainty_1up_MuEffSF_HLT_mu26_ivarmedium_OR_HLT_mu50_QualMedium_IsoNone # 2016
+
                 #sf *= self.chain.lep_0_MUON_EFF_TrigStatUncertainty_1up_MuEffSF_HLT_mu24_imedium_OR_HLT_mu50_QualMedium_IsoGradient
-		sf *= self.chain.lep_0_MUON_EFF_TrigStatUncertainty_1up_MuEffSF_HLT_mu24_imedium_OR_HLT_mu50_QualMedium_IsoNone
+		#sf *= self.chain.lep_0_MUON_EFF_TrigStatUncertainty_1up_MuEffSF_HLT_mu26_ivarmedium_OR_HLT_mu50_QualMedium_IsoNone
 		sf *= self.chain.lep_0_MUON_ISO_STAT_1up_MuEffSF_IsoGradient
                elif self.sys_name=='MUSF_STAT_DN':
+                sf *= self.chain.lep_0_MUON_TTVA_STAT_1down_MuEffSF_TTVA
                 sf *= self.chain.lep_0_MUON_EFF_STAT_1down_MuEffSF_Reco_QualMedium
+
+                if run_number >= 276262 and run_number <= 284484:
+                        sf *= self.chain.lep_0_MUON_EFF_TrigStatUncertainty_1down_MuEffSF_HLT_mu20_iloose_L1MU15_OR_HLT_mu40_QualMedium_IsoNone # 2015
+                if run_number >= 296939 and run_number <= 300287:
+                        sf *= self.chain.lep_0_MUON_EFF_TrigStatUncertainty_1down_MuEffSF_HLT_mu24_imedium_OR_HLT_mu40_QualMedium_IsoNone # 2016
+                if run_number >= 300345 and run_number <= 302872:
+                        sf *= self.chain.lep_0_MUON_EFF_TrigStatUncertainty_1down_MuEffSF_HLT_mu24_imedium_OR_HLT_mu50_QualMedium_IsoNone # 2016
+                if run_number >= 302919:
+                        sf *= self.chain.lep_0_MUON_EFF_TrigStatUncertainty_1down_MuEffSF_HLT_mu26_ivarmedium_OR_HLT_mu50_QualMedium_IsoNone # 2016
+
                 #sf *= self.chain.lep_0_MUON_EFF_TrigStatUncertainty_1down_MuEffSF_HLT_mu24_imedium_OR_HLT_mu50_QualMedium_IsoGradient
-		sf *= self.chain.lep_0_MUON_EFF_TrigStatUncertainty_1down_MuEffSF_HLT_mu24_imedium_OR_HLT_mu50_QualMedium_IsoNone
+		#sf *= self.chain.lep_0_MUON_EFF_TrigStatUncertainty_1down_MuEffSF_HLT_mu26_ivarmedium_OR_HLT_mu50_QualMedium_IsoNone
                 sf *= self.chain.lep_0_MUON_ISO_STAT_1down_MuEffSF_IsoGradient
                if self.sys_name == 'MUSF_SYS_UP':
+                sf *= self.chain.lep_0_MUON_TTVA_SYS_1up_MuEffSF_TTVA
                 sf *= self.chain.lep_0_MUON_EFF_SYS_1up_MuEffSF_Reco_QualMedium
+
+                if run_number >= 276262 and run_number <= 284484:
+                        sf *= self.chain.lep_0_MUON_EFF_TrigSystUncertainty_1up_MuEffSF_HLT_mu20_iloose_L1MU15_OR_HLT_mu40_QualMedium_IsoNone # 2015
+                if run_number >= 296939 and run_number <= 300287:
+                        sf *= self.chain.lep_0_MUON_EFF_TrigSystUncertainty_1up_MuEffSF_HLT_mu24_imedium_OR_HLT_mu40_QualMedium_IsoNone # 2016
+                if run_number >= 300345 and run_number <= 302872:
+                        sf *= self.chain.lep_0_MUON_EFF_TrigSystUncertainty_1up_MuEffSF_HLT_mu24_imedium_OR_HLT_mu50_QualMedium_IsoNone # 2016
+                if run_number >= 302919:
+                        sf *= self.chain.lep_0_MUON_EFF_TrigSystUncertainty_1up_MuEffSF_HLT_mu26_ivarmedium_OR_HLT_mu50_QualMedium_IsoNone # 2016
+
                 #sf *= self.chain.lep_0_MUON_EFF_TrigSystUncertainty_1up_MuEffSF_HLT_mu24_imedium_OR_HLT_mu50_QualMedium_IsoGradient
-		sf *= self.chain.lep_0_MUON_EFF_TrigSystUncertainty_1up_MuEffSF_HLT_mu24_imedium_OR_HLT_mu50_QualMedium_IsoNone
+		#sf *= self.chain.lep_0_MUON_EFF_TrigSystUncertainty_1up_MuEffSF_HLT_mu26_ivarmedium_OR_HLT_mu50_QualMedium_IsoNone
 		sf *= self.chain.lep_0_MUON_ISO_SYS_1up_MuEffSF_IsoGradient
                elif self.sys_name=='MUSF_SYS_DN':
+                sf *= self.chain.lep_0_MUON_TTVA_SYS_1down_MuEffSF_TTVA
                 sf *= self.chain.lep_0_MUON_EFF_SYS_1down_MuEffSF_Reco_QualMedium
+
+                if run_number >= 276262 and run_number <= 284484:
+                        sf *= self.chain.lep_0_MUON_EFF_TrigSystUncertainty_1down_MuEffSF_HLT_mu20_iloose_L1MU15_OR_HLT_mu40_QualMedium_IsoNone # 2015
+                if run_number >= 296939 and run_number <= 300287:
+                        sf *= self.chain.lep_0_MUON_EFF_TrigSystUncertainty_1down_MuEffSF_HLT_mu24_imedium_OR_HLT_mu40_QualMedium_IsoNone # 2016
+                if run_number >= 300345 and run_number <= 302872:
+                        sf *= self.chain.lep_0_MUON_EFF_TrigSystUncertainty_1down_MuEffSF_HLT_mu24_imedium_OR_HLT_mu50_QualMedium_IsoNone # 2016
+                if run_number >= 302919:
+                        sf *= self.chain.lep_0_MUON_EFF_TrigSystUncertainty_1down_MuEffSF_HLT_mu26_ivarmedium_OR_HLT_mu50_QualMedium_IsoNone # 2016
+
                 #sf *= self.chain.lep_0_MUON_EFF_TrigSystUncertainty_1down_MuEffSF_HLT_mu24_imedium_OR_HLT_mu50_QualMedium_IsoGradient
-		sf *= self.chain.lep_0_MUON_EFF_TrigSystUncertainty_1down_MuEffSF_HLT_mu24_imedium_OR_HLT_mu50_QualMedium_IsoNone	
+		#sf *= self.chain.lep_0_MUON_EFF_TrigSystUncertainty_1down_MuEffSF_HLT_mu26_ivarmedium_OR_HLT_mu50_QualMedium_IsoNone	
                 sf *= self.chain.lep_0_MUON_ISO_SYS_1down_MuEffSF_IsoGradient
           else: pass
         if self.key: 
@@ -197,12 +276,19 @@ class MuonSFIsoGrad(pyframe.core.Algorithm):
         if "mc" in self.sampletype:
 
 	  #sf=1.0
+          run_number = self.chain.NOMINAL_pileup_random_run_number
 
           sf *= self.chain.lep_0_NOMINAL_MuEffSF_Reco_QualMedium
+	  sf*= self.chain.lep_0_NOMINAL_MuEffSF_TTVA
 
-	  #sf *= self.chain.lep_0_NOMINAL_MuEffSF_HLT_mu20_iloose_L1MU15_OR_HLT_mu40_QualMedium_IsoIsoNone # v12 2015
-          #sf *= self.chain.lep_0_NOMINAL_MuEffSF_HLT_mu20_iloose_L1MU15_OR_HLT_mu40_QualMedium_IsoNone # 2015
-          sf *= self.chain.lep_0_NOMINAL_MuEffSF_HLT_mu24_imedium_OR_HLT_mu50_QualMedium_IsoNone # 2016
+          if run_number >= 276262 and run_number <= 284484:
+          	sf *= self.chain.lep_0_NOMINAL_MuEffSF_HLT_mu20_iloose_L1MU15_OR_HLT_mu40_QualMedium_IsoNone # 2015
+          if run_number >= 296939 and run_number <= 300287:
+                sf *= self.chain.lep_0_NOMINAL_MuEffSF_HLT_mu24_imedium_OR_HLT_mu40_QualMedium_IsoNone # 2016
+          if run_number >= 300345 and run_number <= 302872:
+                sf *= self.chain.lep_0_NOMINAL_MuEffSF_HLT_mu24_imedium_OR_HLT_mu50_QualMedium_IsoNone # 2016
+          if run_number >= 302919: 
+          	sf *= self.chain.lep_0_NOMINAL_MuEffSF_HLT_mu26_ivarmedium_OR_HLT_mu50_QualMedium_IsoNone # 2016
 
 	  #print "pt(mu) %lf, antiIso trig SF %lf, reco SF %lf " % (self.chain.lep_0_pt, self.chain.lep_0_NOMINAL_MuEffSF_HLT_mu24_imedium_OR_HLT_mu50_QualMedium_IsoNone, self.chain.lep_0_NOMINAL_MuEffSF_Reco_QualMedium)          
 
@@ -210,19 +296,65 @@ class MuonSFIsoGrad(pyframe.core.Algorithm):
                sf = 1.0
                if self.sys_name == 'MUSF_SYS_UP':
                 sf *= self.chain.lep_0_MUON_EFF_SYS_1up_MuEffSF_Reco_QualMedium
-		sf *= self.chain.lep_0_MUON_EFF_TrigSystUncertainty_1up_MuEffSF_HLT_mu24_imedium_OR_HLT_mu50_QualMedium_IsoNone
+
+		if run_number >= 276262 and run_number <= 284484:
+			sf *= self.chain.lep_0_MUON_EFF_TrigSystUncertainty_1up_MuEffSF_HLT_mu20_iloose_L1MU15_OR_HLT_mu40_QualMedium_IsoNone # 2015
+		if run_number >= 296939 and run_number <= 300287:
+			sf *= self.chain.lep_0_MUON_EFF_TrigSystUncertainty_1up_MuEffSF_HLT_mu24_imedium_OR_HLT_mu40_QualMedium_IsoNone # 2016
+		if run_number >= 300345 and run_number <= 302872:
+			sf *= self.chain.lep_0_MUON_EFF_TrigSystUncertainty_1up_MuEffSF_HLT_mu24_imedium_OR_HLT_mu50_QualMedium_IsoNone # 2016
+		if run_number >= 302919:
+			sf *= self.chain.lep_0_MUON_EFF_TrigSystUncertainty_1up_MuEffSF_HLT_mu26_ivarmedium_OR_HLT_mu50_QualMedium_IsoNone # 2016
+
+		#sf *= self.chain.lep_0_MUON_EFF_TrigSystUncertainty_1up_MuEffSF_HLT_mu26_ivarmedium_OR_HLT_mu50_QualMedium_IsoNone
+                sf *= self.chain.lep_0_MUON_TTVA_SYS_1up_MuEffSF_TTVA
 
                elif self.sys_name=='MUSF_SYS_DN':
                 sf *= self.chain.lep_0_MUON_EFF_SYS_1down_MuEffSF_Reco_QualMedium
-                sf *= self.chain.lep_0_MUON_EFF_TrigSystUncertainty_1down_MuEffSF_HLT_mu24_imedium_OR_HLT_mu50_QualMedium_IsoNone
+
+                if run_number >= 276262 and run_number <= 284484:
+                        sf *= self.chain.lep_0_MUON_EFF_TrigSystUncertainty_1down_MuEffSF_HLT_mu20_iloose_L1MU15_OR_HLT_mu40_QualMedium_IsoNone # 2015
+                if run_number >= 296939 and run_number <= 300287:
+                        sf *= self.chain.lep_0_MUON_EFF_TrigSystUncertainty_1down_MuEffSF_HLT_mu24_imedium_OR_HLT_mu40_QualMedium_IsoNone # 2016
+                if run_number >= 300345 and run_number <= 302872:
+                        sf *= self.chain.lep_0_MUON_EFF_TrigSystUncertainty_1down_MuEffSF_HLT_mu24_imedium_OR_HLT_mu50_QualMedium_IsoNone # 2016
+                if run_number >= 302919:
+                        sf *= self.chain.lep_0_MUON_EFF_TrigSystUncertainty_1down_MuEffSF_HLT_mu26_ivarmedium_OR_HLT_mu50_QualMedium_IsoNone # 2016
+                
+
+                #sf *= self.chain.lep_0_MUON_EFF_TrigSystUncertainty_1down_MuEffSF_HLT_mu26_ivarmedium_OR_HLT_mu50_QualMedium_IsoNone
+                sf *= self.chain.lep_0_MUON_TTVA_SYS_1down_MuEffSF_TTVA
 
                if self.sys_name == 'MUSF_STAT_UP':
                 sf *= self.chain.lep_0_MUON_EFF_STAT_1up_MuEffSF_Reco_QualMedium
-                sf *= self.chain.lep_0_MUON_EFF_TrigStatUncertainty_1up_MuEffSF_HLT_mu24_imedium_OR_HLT_mu50_QualMedium_IsoNone
+
+                if run_number >= 276262 and run_number <= 284484:
+                        sf *= self.chain.lep_0_MUON_EFF_TrigStatUncertainty_1up_MuEffSF_HLT_mu20_iloose_L1MU15_OR_HLT_mu40_QualMedium_IsoNone # 2015
+                if run_number >= 296939 and run_number <= 300287:
+                        sf *= self.chain.lep_0_MUON_EFF_TrigStatUncertainty_1up_MuEffSF_HLT_mu24_imedium_OR_HLT_mu40_QualMedium_IsoNone # 2016
+                if run_number >= 300345 and run_number <= 302872:
+                        sf *= self.chain.lep_0_MUON_EFF_TrigStatUncertainty_1up_MuEffSF_HLT_mu24_imedium_OR_HLT_mu50_QualMedium_IsoNone # 2016
+                if run_number >= 302919:
+                        sf *= self.chain.lep_0_MUON_EFF_TrigStatUncertainty_1up_MuEffSF_HLT_mu26_ivarmedium_OR_HLT_mu50_QualMedium_IsoNone # 2016
+                
+                #sf *= self.chain.lep_0_MUON_EFF_TrigStatUncertainty_1up_MuEffSF_HLT_mu26_ivarmedium_OR_HLT_mu50_QualMedium_IsoNone
+                sf *= self.chain.lep_0_MUON_TTVA_STAT_1up_MuEffSF_TTVA
 
                elif self.sys_name=='MUSF_STAT_DN':
                 sf *= self.chain.lep_0_MUON_EFF_STAT_1down_MuEffSF_Reco_QualMedium
-                sf *= self.chain.lep_0_MUON_EFF_TrigStatUncertainty_1down_MuEffSF_HLT_mu24_imedium_OR_HLT_mu50_QualMedium_IsoNone
+
+                if run_number >= 276262 and run_number <= 284484:
+                        sf *= self.chain.lep_0_MUON_EFF_TrigStatUncertainty_1down_MuEffSF_HLT_mu20_iloose_L1MU15_OR_HLT_mu40_QualMedium_IsoNone # 2015
+                if run_number >= 296939 and run_number <= 300287:
+                        sf *= self.chain.lep_0_MUON_EFF_TrigStatUncertainty_1down_MuEffSF_HLT_mu24_imedium_OR_HLT_mu40_QualMedium_IsoNone # 2016
+                if run_number >= 300345 and run_number <= 302872:
+                        sf *= self.chain.lep_0_MUON_EFF_TrigStatUncertainty_1down_MuEffSF_HLT_mu24_imedium_OR_HLT_mu50_QualMedium_IsoNone # 2016
+                if run_number >= 302919:
+                        sf *= self.chain.lep_0_MUON_EFF_TrigStatUncertainty_1down_MuEffSF_HLT_mu26_ivarmedium_OR_HLT_mu50_QualMedium_IsoNone # 2016
+
+
+                #sf *= self.chain.lep_0_MUON_EFF_TrigStatUncertainty_1down_MuEffSF_HLT_mu26_ivarmedium_OR_HLT_mu50_QualMedium_IsoNone
+                sf *= self.chain.lep_0_MUON_TTVA_STAT_1down_MuEffSF_TTVA
 
           else: pass
           
@@ -269,7 +401,7 @@ class TauSF(pyframe.core.Algorithm):
 	  sf *= self.chain.tau_0_NOMINAL_TauEffSF_JetBDTmedium
  	  sf *= self.chain.tau_0_NOMINAL_TauEffSF_reco
 	  #sf *= self.chain.tau_0_NOMINAL_TauEffSF_HadTauEleOLR_tauhad
-       	  sf *= self.chain.tau_0_NOMINAL_TauEffSF_VeryLooseLlhEleOLR_electron
+       	  sf *= self.chain.tau_0_NOMINAL_TauEffSF_LooseLlhEleOLR_electron
  	  #print "tau(mu) %lf, tau id trig SF %lf, tau reco SF %lf, tau olr SF %lf " % (self.chain.tau_0_pt, self.chain.tau_0_NOMINAL_TauEffSF_JetBDTmedium, self.chain.tau_0_NOMINAL_TauEffSF_reco, self.chain.tau_0_NOMINAL_TauEffSF_HadTauEleOLR_tauhad)
         
 	  if self.scale:
