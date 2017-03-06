84c9a84d08eada47e61a34215552cb178b35d1cf
Lara Mason <laram1@vm-115-146-84-202.melbourne.rc.nectar.org.au>
2017-03-06 12:34:37 +0000

    update

--- test/hist.sh
+++ test/hist.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ## Batch
-python ../ztautau/run/j.plotter.py --input /coepp/cephfs/mel/laram1/v25_2016/nominal/physics_Main_00302380.root --sampletype="data"
+python ../ztautau/run/j.plotter.py --input /coepp/cephfs/mel/laram1/v28/2016/nominal/physics_Main_00300279.root --sampletype="data"
 #python ../ztautau/run/j.plotter.py --input /coepp/cephfs/mel/laram1/v25_2016/nominal/PoPy8_Ztt_truth.root --sampletype="mcTruth"
 #python ../ztautau/run/j.plotter.py --input /coepp/cephfs/mel/laram1/v25_2016/nominal/PoPy_P2012_STSchan_noAllHad_atop_antitruth.root  --sampletype="mcAntiTruth"
-#python ../ztautau/run/j.plotter.py --input /coepp/cephfs/mel/laram1/v25_2015/nominal/PoPy8_Zmumu.root --sampletype="mc"
+#python ../ztautau/run/j.plotter.py --input /coepp/cephfs/mel/laram1/v26/nominal/PoPy8_Ztt_truth.root --sampletype="mcTruth"
