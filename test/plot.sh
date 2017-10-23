3fb77b422fda93a4ba75c99ceaca5477ba3f6bc6
Brian Le <brianl@student.unimelb.edu.au>
2017-10-23 07:51:13 +0000

    v3 update

--- test/plot.sh
+++ test/plot.sh
@@ -2,7 +2,7 @@
 
 # Strings are passed to the script but this is redundant!
 
-python ../ztautau/scripts/merge.py --var="tau_0_pt" --reg="presel_mu16inc" --lab="TEST REGION" --icut="11" --input="/coepp/cephfs/share/atlas/LFV/FourthTestNN" --output="./" --makeplot=False
+python ../ztautau/scripts/merge.py --var="tau_0_pt" --reg="presel_el16inc" --lab="TEST REGION" --icut="12" --input="/coepp/cephfs/share/atlas/LFV/test_v1" --output="./" --makeplot=True
 
 
 
